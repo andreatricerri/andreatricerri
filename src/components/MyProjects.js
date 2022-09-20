997e167843274f266793207a126abe4a9b27b695
Andrea Tricerri <107701928+andreatricerri@users.noreply.github.com>
2022-09-20 15:23:46 +0100

    Update MyProjects.js

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -11,7 +11,7 @@ function MyProjects() {
             <div className="quizDesc">
             <h4 className="projectsTitle">Quiz</h4>
             <h6 id="fontSize">Let's see how much you know about me, you can win a skiing holiday in the Maldives. I am sure you cannot do 100%.</h6>
-            <a href="https://github.com/andreatricerri/quizApp" target='_blank' class="myButtonProjects" >Go to the Quiz</a>
+            <a href="https://quiz-app-virid-ten.vercel.app" target='_blank' class="myButtonProjects" >Go to the Quiz</a>
             </div>
             </div>
             <div className='calculator col-lg-6'>
@@ -20,7 +20,7 @@ function MyProjects() {
             <div className="calcDesc">
             <h4 className="projectsTitle">Calculator</h4>
             <h6 id="fontSize">Everyone needs a calculator at hand, we both know it. I will lend you mine: here it is!</h6>
-            <a href="https://github.com/andreatricerri/calculator" target='_blank' class="myButtonProjects">Your Calculator</a>
+            <a href="https://calculator-coral-theta.vercel.app" target='_blank' class="myButtonProjects">Your Calculator</a>
             </div>
             </div>
         </section>
@@ -31,7 +31,7 @@ function MyProjects() {
             <div className="pwDesc">
             <h4 className="projectsTitle">Password Generator</h4>
             <h6 id="fontSize">Your password is no longer secure. Luckily you can now change it in just few clicks. </h6>
-            <a href="https://github.com/andreatricerri/passwordGenerator" target='_blank' class="myButtonProjects">Change your PW</a>
+            <a href="https://password-generator-eight-beta.vercel.app" target='_blank' class="myButtonProjects">Change your PW</a>
             </div>
             </div>
             <div className='countdown col-lg-6'>
@@ -40,7 +40,7 @@ function MyProjects() {
             <div className="countDesc">
             <h4 className="projectsTitle">Countdown</h4>
             <h6 id="fontSize">Do you know how many days are left until Christmas? And Easter? Ok, but i am sure you don't know this countdown.</h6>
-            <a href="https://github.com/andreatricerri/countdown" target='_blank' class="myButtonProjects">Hey, check here!</a>
+            <a href="https://countdown-timer-ruby-rho.vercel.app" target='_blank' class="myButtonProjects">Hey, check here!</a>
             </div>
             </div>
         </section>
